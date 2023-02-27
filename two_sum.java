a40592a239ee4d144c4d27cbbfffd5a999cfbc8a
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2023-02-27 18:03:42 +0530

    two sum and merge binary tree

---
+++ two_sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[], int target) {
+        int[] output = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int y = i + 1; y < nums.length; y++) {
+                if (nums[i] + nums[y] == target) {
+                    output[0] = i;
+                    output[1] = y;
+                    return output;
+                }
+            }
+        }
+        return output;
+    }
+}
